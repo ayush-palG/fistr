264d7532fc8f51cee4a082ff1b3b225797d254a9
ayush-palG <ayushpal.9470@gmail.com>
2025-05-05 14:53:37 +0530

    (#5) change the factorial example wrt new fistr

--- examples/build.sh
+++ examples/build.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-cc -Wall -Wextra -ggdb -fsanitize=address -o factorial factorial.c
 cc -Wall -Wextra -ggdb -fsanitize=address -o nCk nCk.c
 cc -Wall -Wextra -ggdb -fsanitize=address -I ../src/ -o fib fib.c
+cc -Wall -Wextra -ggdb -fsanitize=address -I ../src/ -o factorial factorial.c
--- examples/factorial.c
+++ examples/factorial.c
@@ -1,20 +1,18 @@
 #define FISTR_IMPLEMENTATION
-#include "../fistr.h"
+#include "fistr.h"
 
 int main(void)
 {
-  Fistr a, b, c;
-
-  b = int_as_fistr(1);
-  for (size_t i = 1; i <= 10; ++i) {
-    a = int_as_fistr(i);
-    c = b;
-    b = fistr_mult(&b, &a);
-    print_fistr(&b);
-    free(a.str);
-    free(c.str);
+  Fistr start = int_as_fistr(1);
+  for (size_t i = 1; i <= 100; ++i) {
+    Fistr next = int_as_fistr(i);
+    fistr_mul(&start, &next);
+    printf("%2zu: ", i);
+    fistr_print(start);
+    free(next.string.buffer);
   }
 
-  free(b.str);
+  free(start.string.buffer);
+  
   return 0;
 }
